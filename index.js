90d4e7eade73c8430312a30ecd64462b8538cd8d
coder-us-mj <coder.us.jm@gmail.com>
2025-01-07 13:40:17 +0530

    commitchanges

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
 
-        var typed = new typed(".text",{
-            String:["Java Developer","Web Designer","Backend Developer"],
-            typeSpeed: 100,
-            backSpeed: 100,
-            backDelay: 1000,
-            loop: true            
-        })
-        var tablinks = document.getElementsByClassName("tab-links");
-        var tabcontents = document.getElementsByClassName("tab-contents");
-        function opentab(tabname) {
-            for (tablink of tablinks) {
-                tablink.classList.remove("active-link")
-            }
-            for (tabcontent of tabcontents) {
-                tabcontent.classList.remove("active-tab")
-            }
-            event.currentTarget.classList.add("active-link")
-            document.getElementById(tabname).classList.add("active-tab")
-        }
+    //     var typed = new typed(".text",{
+    //         String:["Java Developer","Web Designer","Backend Developer"],
+    //         typeSpeed: 100,
+    //         backSpeed: 100,
+    //         backDelay: 1000,
+    //         loop: true            
+    //     })
+    //     var tablinks = document.getElementsByClassName("tab-links");
+    //     var tabcontents = document.getElementsByClassName("tab-contents");
+    //     function opentab(tabname) {
+    //         for (tablink of tablinks) {
+    //             tablink.classList.remove("active-link")
+    //         }
+    //         for (tabcontent of tabcontents) {
+    //             tabcontent.classList.remove("active-tab")
+    //         }
+    //         event.currentTarget.classList.add("active-link")
+    //         document.getElementById(tabname).classList.add("active-tab")
+    //     }
 
-        var Sidemenu = document.getElementById("sidemenu");
+    //     var Sidemenu = document.getElementById("sidemenu");
         
-        function openmenu(){
-            Sidemenu.style.right = "0px";
-        }
-        function closemenu(){
-            Sidemenu.style.right = "-200px";
-        }
+    //     function openmenu(){
+    //         Sidemenu.style.right = "0px";
+    //     }
+    //     function closemenu(){
+    //         Sidemenu.style.right = "-200px";
+    //     }
 
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbwm_05LB1zF0pbEXEYNGgyMZcJV5hNnid4iiWnK4-HY-GtzjlKNWPqHJVcFx-mVtiUa/exec'
-    const form = document.forms['submit-to-google-sheet']
-    const msg=document.getElementById("msg")
-    form.addEventListener('submit', e => {
-      e.preventDefault()
-      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-        .then(response => {
-            msg.innerHTML ="Message Sent Successfully"
-            setTimeout(function() {
-                msg.innerHTML = ""
-            },5000)
-            form.reset()
-        })
-        .catch(error => console.error('Error!', error.message))
-    })
+    // const scriptURL = 'https://script.google.com/macros/s/AKfycbwm_05LB1zF0pbEXEYNGgyMZcJV5hNnid4iiWnK4-HY-GtzjlKNWPqHJVcFx-mVtiUa/exec'
+    // const form = document.forms['submit-to-google-sheet']
+    // const msg=document.getElementById("msg")
+    // form.addEventListener('submit', e => {
+    //   e.preventDefault()
+    //   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    //     .then(response => {
+    //         msg.innerHTML ="Message Sent Successfully"
+    //         setTimeout(function() {
+    //             msg.innerHTML = ""
+    //         },5000)
+    //         form.reset()
+    //     })
+    //     .catch(error => console.error('Error!', error.message))
+    // })
